2b711d85ba57a705596339481f809a316e61aff6
Vitor Lira <sblvitor@gmail.com>
2026-09-05 00:05:59 -0400

    initial setup

---
+++ src/script.js
@@ -0,0 +1,4 @@
+import './style.css'
+import Orchestrator from './threejs/Orchestrator'
+
+const orchestrator = new Orchestrator(document.querySelector('canvas.webgl'))
---
+++ src/threejs/Orchestrator.js
@@ -0,0 +1,5 @@
+export default class Orchestrator {
+  constructor(canvas) {
+    this.canvas = canvas
+  }
+}
