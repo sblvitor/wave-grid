da639ae4918c870aeb5c0fa11b2a7b5c97435c7a
Vitor Lira <sblvitor@gmail.com>
2026-09-05 01:19:04 -0400

    add util classes

--- src/threejs/Orchestrator.js
+++ src/threejs/Orchestrator.js
@@ -1,5 +1,35 @@
+import * as THREE from 'three'
+import Sizes from "./utils/Sizes"
+import Time from "./utils/Time"
+
 export default class Orchestrator {
+  
   constructor(canvas) {
+    
+    // Options
     this.canvas = canvas
+
+    // Setup
+    this.sizes = new Sizes()
+    this.time = new Time()
+    this.scene = new THREE.Scene()
+ 
+    // Resize event
+    this.sizes.emitter.on('resize', () => {
+      this.resize()
+    })
+
+    // Tick event
+    this.time.emitter.on('tick', () => {
+      this.update()
+    })
   }
+
+  resize() {
+  }
+
+  update() {
+
+  }
+
 }
