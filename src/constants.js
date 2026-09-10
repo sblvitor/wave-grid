571cf32eb5f5603d3ae9f9ff359d8f9006cd3ce7
Vitor Lira <sblvitor@gmail.com>
2026-09-10 15:03:19 -0400

    add dynamic content

---
+++ src/constants.js
@@ -0,0 +1,18 @@
+const lastAboutWord = [
+  'learning',
+  'building',
+  'creating',
+  'exploring'
+]
+
+const content = {
+  about: `Software Engineer from Brazil with a passion for creative, immersive and memorable experiences on the web. I enjoy turning complex ideas into something people can feel — always ${lastAboutWord[0]}.`,
+  experience: 'Experience text',
+  projects: 'Projects text',
+  contact: 'Contacts',
+}
+
+export {
+  content,
+  lastAboutWord
+}
--- src/script.js
+++ src/script.js
@@ -1,5 +1,6 @@
 import './style.css'
 import Orchestrator from './threejs/Orchestrator'
+import { content } from './constants'
 
 new Orchestrator(document.querySelector('canvas.webgl'))
 
@@ -17,4 +18,16 @@ themeToggleBtn.addEventListener('click', () => {
   const newTheme = currentTheme === 'dark' ? 'light' : 'dark'
   root.setAttribute('data-theme', newTheme)
   localStorage.setItem('theme', newTheme)
+})
+
+// Content management
+const h1 = document.querySelector('.content h1')
+h1.textContent = content.about
+
+const navButtons = document.querySelectorAll('.nav-link')
+navButtons.forEach((button) => {
+  button.addEventListener('click', () => {
+    const section = button.dataset.section
+    h1.textContent = content[section]
+  })
 })
